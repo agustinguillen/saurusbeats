c8568808c37a987f1135c39666375bf0c63deaf2
Agustin Guillen <agustinnguillen@gmail.com>
2021-03-12 18:10:54 -0300

    Parar reproducción de videos al cerrar modales

---
+++ js/pause.js
@@ -0,0 +1,19 @@
+$("#modalCromo").on('hidden.bs.modal', function (e) {
+    $("#modalCromo iframe").attr("src", $("#modalCromo iframe").attr("src"));
+});
+
+$("#modalMarea").on('hidden.bs.modal', function (e) {
+    $("#modalMarea iframe").attr("src", $("#modalMarea iframe").attr("src"));
+});
+
+$("#modalSummer").on('hidden.bs.modal', function (e) {
+    $("#modalSummer iframe").attr("src", $("#modalSummer iframe").attr("src"));
+});
+
+$("#modalSick").on('hidden.bs.modal', function (e) {
+    $("#modalSick iframe").attr("src", $("#modalSick iframe").attr("src"));
+});
+
+$("#modalRadioactive").on('hidden.bs.modal', function (e) {
+    $("#modalRadioactive iframe").attr("src", $("#modalRadioactive iframe").attr("src"));
+});
